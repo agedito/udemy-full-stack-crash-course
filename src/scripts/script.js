268abfbc0bdeac2108b9d2d5052286235fe3000f
agedito <agedito@gmail.com>
2022-12-19 06:52:29 +0100

    hidden / show form

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,11 @@
-console.log("Hola carabola");
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+
+btn.addEventListener("click", function () {
+  console.log("Click", form);
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+  } else {
+    form.classList.add("hidden");
+  }
+});
