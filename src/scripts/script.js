a11bd6dcf7c3614d85b02c539978a92f863bf726
agedito <agedito@gmail.com>
2022-12-19 07:08:10 +0100

    finsih toggle form visibility

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,11 +1,14 @@
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
 
-btn.addEventListener("click", function () {
-  console.log("Click", form);
+btn.addEventListener("click", toggleFormVisibility);
+
+function toggleFormVisibility() {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
+    btn.textContent = "Close";
   } else {
     form.classList.add("hidden");
+    btn.textContent = "Share a fact";
   }
-});
+}
